cda6678b65111812a562b063065da126411f24a2
Loren Lugosch <lugosch@orthanc.ece.mcgill.ca>
2019-12-13 12:24:02 -0500

    improved architecture with conv

--- main.py
+++ main.py
@@ -21,15 +21,26 @@ torch.manual_seed(config.seed); np.random.seed(config.seed)
 
 # Initialize model
 model = CTCModel(config=config)
+print(model)
 
+# Generate datasets
+train_dataset, valid_dataset, test_dataset = get_ASR_datasets(config)
+
+trainer = Trainer(model=model, config=config)
+if restart: trainer.load_checkpoint()
+
+#######################
+#from data import CollateWavsASR
+#c = CollateWavsASR()
+#indices = [247139, 46541, 53391, 188748, 276074, 196684, 211271, 159381, 137485, 204068, 273793, 4477, 68569, 240939, 233307, 52096, 72788, 168682, 131440, 50584, 157714, 190209, 79150, 159430, 252179, 21565, 44222, 277035, 6492, 165880, 192524, 262075, 254863, 173450, 80020, 232210, 4045, 89528, 126462, 67697, 267582, 119138, 252103, 275955, 179933, 238829, 228952, 253370, 87137, 251881, 213141, 139106, 20439, 174443, 193189, 50926, 209070, 25481, 221399, 36916, 54646, 265435, 175335, 59256]
+#b = [ train_dataset.__getitem__(idx) for idx in indices]
+#batch = c.__call__(b)
+#x,y,T,U,idxs = batch
+#log_probs = model(x,y,T,U)
+#######################
+
+# Train the final model
 if train:
-	# Generate datasets
-	train_dataset, valid_dataset, test_dataset = get_ASR_datasets(config)
-
-	# Train the final model
-	trainer = Trainer(model=model, config=config)
-	if restart: trainer.load_checkpoint()
-
 	for epoch in range(config.num_epochs):
 		print("========= Epoch %d of %d =========" % (epoch+1, config.num_epochs))
 		train_WER, train_loss = trainer.train(train_dataset)
@@ -38,8 +49,9 @@ if train:
 		print("========= Results: epoch %d of %d =========" % (epoch+1, config.num_epochs))
 		print("train WER: %.2f| train loss: %.2f| valid WER: %.2f| valid loss: %.2f\n" % (train_WER, train_loss, valid_WER, valid_loss) )
 
-		trainer.save_checkpoint()
+		trainer.save_checkpoint(WER=valid_WER)
 
+	trainer.load_best_model()
 	test_WER, test_loss = trainer.test(test_dataset, set="test")
 	print("========= Test results =========")
 	print("test WER: %.2f| test loss: %.2f \n" % (test_WER, test_loss) )
--- models.py
+++ models.py
@@ -17,6 +17,12 @@ class CTCModel(torch.nn.Module):
 		labels = ["a" for _ in range(self.num_outputs)] # doesn't matter, just need 1-char labels
 		self.decoder = ctcdecode.CTCBeamDecoder(labels, blank_id=self.blank_index, beam_width=config.beam_width)
 
+	def load_pretrained(self, model_path=None):
+		if model_path == None:
+			model_path = os.path.join(self.checkpoint_path, "model_state.pth")
+		device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+		self.load_state_dict(torch.load(model_path, map_location=device))
+
 	def forward(self, x, y, T, U):
 		"""
 		returns log probs for each example
@@ -34,9 +40,6 @@ class CTCModel(torch.nn.Module):
 		downsampling_factor = max(T) / encoder_out.shape[1]
 		T = [round(t / downsampling_factor) for t in T]
 		encoder_out = encoder_out.transpose(0,1) # (N, T, #labels) --> (T, N, #labels)
-		if max(U) > max(T):
-			print("Error! Output longer than input!")
-			sys.exit()
 		log_probs = -torch.nn.functional.ctc_loss(	log_probs=encoder_out,
 								targets=y,
 								input_lengths=T,
@@ -94,15 +97,42 @@ class TransducerModel(torch.nn.Module):
 		"""
 		return 1
 
+class Conv(torch.nn.Module):
+	def __init__(self, in_dim, out_dim, filter_length, stride):
+		super(Conv, self).__init__()
+		self.conv = torch.nn.Conv1d(	in_channels=in_dim,
+						out_channels=out_dim,
+						kernel_size=filter_length,
+						stride=stride
+		)
+		self.filter_length = filter_length
+
+	def forward(self, x):
+		out = x.transpose(1,2)
+		left_padding = int(self.filter_length/2)
+		right_padding = int(self.filter_length/2)
+		out = torch.nn.functional.pad(out, (left_padding, right_padding))
+		out = self.conv(out) / self.filter_length # HACK seeing if this helps
+		out = out.transpose(1,2)
+		return out
+
 class Encoder(torch.nn.Module):
 	def __init__(self, config):
 		super(Encoder, self).__init__()
 		self.layers = []
 
 		# compute FBANK features from waveform
-		layer = ComputeFBANK()
+		layer = ComputeFBANK(config)
 		self.layers.append(layer)
-		out_dim = 40
+		out_dim = config.num_mel_bins
+
+		# convolution to give future context
+		context_len = 11 # 11 fbank frames
+		layer = Conv(in_dim=out_dim, out_dim=config.num_hidden, filter_length=context_len, stride=2)
+		self.layers.append(layer)
+		out_dim = config.num_hidden
+		layer = torch.nn.LeakyReLU(0.125)
+		self.layers.append(layer)
 
 		for idx in range(config.num_layers):
 			# recurrent
@@ -117,13 +147,23 @@ class Encoder(torch.nn.Module):
 			self.layers.append(layer)
 
 			# dropout
-			layer = torch.nn.Dropout()
+			layer = torch.nn.Dropout(p=0.2)
+			self.layers.append(layer)
+
+			# fully-connected
+			layer = torch.nn.Linear(out_dim, config.num_hidden)
+			out_dim = config.num_hidden
+			self.layers.append(layer)
+			layer = torch.nn.LeakyReLU(0.125)
 			self.layers.append(layer)
 
 			# downsample
-			if idx % 2 == 0:
+			if idx == 0:
 				layer = Downsample(method="avg", factor=2, axis=1)
 				self.layers.append(layer)
+
+		#layer = torch.nn.LeakyReLU(0.125)
+		#self.layers.append(layer)
 
 		# final classifier
 		self.num_outputs = config.num_tokens + 1 # for blank symbol
@@ -137,7 +177,7 @@ class Encoder(torch.nn.Module):
 		self.layers = torch.nn.ModuleList(self.layers)
 
 	def forward(self, x, T):
-		out = x
+		out = (x,T)
 		for layer in self.layers:
 			out = layer(out)
 
@@ -196,25 +236,42 @@ class Downsample(torch.nn.Module):
 			return torch.nn.functional.max_pool1d(x.transpose(self.axis, 2), kernel_size=self.factor, ceil_mode=True).transpose(self.axis, 2)
 
 class ComputeFBANK(torch.nn.Module):
-	def __init__(self):
+	def __init__(self, config):
 		super(ComputeFBANK,self).__init__()
+		self.num_mel_bins = config.num_mel_bins
+		self.subtract_mean = config.normalize_fbank
 		self.fbank_params = {
                     "channel": 0,
                     "dither": 0.0,
                     "window_type": "hanning",
-                    "num_mel_bins":80,
+                    "num_mel_bins":self.num_mel_bins,
                     "remove_dc_offset": False,
                     "round_to_power_of_two": False,
                     "sample_frequency":16000.0,
                 }
 
-	def forward(self, x, subtract_mean=False):
+	def forward(self, input):
 		"""
+		input : (x,T)
 		x : waveforms
-		returns : FBANK feature vectors
+		T : durations
+		returns : (normalized) FBANK feature vectors
 		"""
-		fbank = torch.stack([torchaudio.compliance.kaldi.fbank(xx.unsqueeze(0), **self.fbank_params) for xx in x])
-		if subtract_mean: fbank = fbank - fbank.mean(1)
+		#fbank = torch.stack([torchaudio.compliance.kaldi.fbank(xx.unsqueeze(0), **self.fbank_params) for xx in x])
+		fbanks = []
+		x,T = input
+		batch_size = len(x)
+		for idx in range(batch_size):
+			fbank_ = torchaudio.compliance.kaldi.fbank(x[idx].unsqueeze(0), **self.fbank_params)
+			fbanks.append(fbank_)
+
+		if self.subtract_mean:
+			downsample_factor = max(T) / fbanks[0].shape[1]
+			for idx in range(batch_size):
+				T_ = int(T[idx] / downsample_factor)
+				fbanks[idx][:T_, :] -= fbanks[idx][:T_, :].mean(0)
+
+		fbank = torch.stack(fbanks)
 		return fbank
 
 class SpecAugment(torch.nn.Module):
